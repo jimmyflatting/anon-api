02671e022589f300e4e443a7e57fb176d12818e7
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-10-27 17:41:23 +0100

    debugger

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"] # os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
  
